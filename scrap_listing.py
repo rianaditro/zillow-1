66fbad1e552a375390a84dfdb11b611da93ceda5
rianaditro <rianaditro@gmail.com>
2024-02-21 17:06:23 +0700

    rent has no lastUpdated

--- scrap_listing.py
+++ scrap_listing.py
@@ -1,47 +1,104 @@
-import hrequests
+"""
+this scraping flow is :
+from encodedZuid : X1-ZUza7jt9n9hzwp_5gnlt (you get this from every broker profile)
+you go to :
+https://www.zillow.com/profile-page/api/public/v1/map-results?encodedZuid=X1-ZUza7jt9n9hzwp_5gnlt
+
+there are list like this :
+[{"lat":40.85839,"long":-74.1519,"statusType":"SOLD","zpid":39726607},
+{"lat":40.97063,"long":-74.31918,"statusType":"SOLD","zpid":2055704942},
+{"lat":40.94351,"long":-74.21878,"statusType":"SOLD","zpid":39791397}]
+
+grab the zpid then go to :
+https://www.zillow.com/homedetails/63738098_zpid/
+
+its redirected to :
+https://www.zillow.com/homedetails/263-Boulevard-Pompton-Plains-NJ-07444/63738098_zpid/
+
+Happy Scraping
+"""
+
+from scrap_broker import get_html, get_data
+
+import json
+import concurrent.futures
+import pandas
 
 
-def send_request():
-    url = "https://www.zillow.com/profile/RobertDekanski"
-    r = hrequests.get(url)
-    print(r.status_code)
-    print(r.text)
+def get_house_urls(text):
+    list_of_dict = convert_to_dicts(text)
+    zpid_list = [f'https://www.zillow.com/homedetails/{item["zpid"]}_zpid' for item in list_of_dict if item["statusType"]=="FOR_SALE" or item["statusType"]=="FOR_RENT"]
+    return tuple(zpid_list)
 
-send_request()
+def convert_to_dicts(text):
+     text = text.replace("<html><head></head><body>[","").replace("]</body></html>","").replace("},","}},").split("},")
+     result = [json.loads(item) for item in text]
+     return result
 
-# if __name__=="__main__":
-#     encodedZuid = "X1-ZUz9q7ndwbqqrt_aroy1"
-#     size = 100
+def parse_home_details(url)->dict:
+     text = get_data(url)["props"]["pageProps"]["componentProps"]["gdpClientCache"]
+     dict_text = json.loads(text)
 
-#     # text =
+     result = {
+          "zpid":"",
+          "homeStatus":"",
+          "price":"",
+          "homeType":"",
+          "streetAddress":"",
+          "latitude":"",
+          "longitude":"",
+          "bedrooms":"",
+          "bathrooms":"",
+          "yearBuilt":"",
+          "daysOnZillow":"",
+          "hdpUrl":"",
+          "agentName":"",
+          "agentPhoneNumber":"",
+          "brokerName":"",
+          "brokerPhoneNumber":"",
+          "lastUpdated":""
+     }
+     dict_text = list(dict_text.items())[0][1]["property"]
 
-#     url = f"https://www.zillow.com/profile-page/api/public/v1/active-listings?encodedZuid={encodedZuid}&size={size}"
-#     profile = "https://www.zillow.com/profile/RobertDekanski"
-#     maps = "https://www.zillow.com/profile-page/api/public/v1/map-results?encodedZuid=X1-ZUz9q7ndwbqqrt_aroy1"
+     get_only = ["zpid","homeStatus","bedrooms","bathrooms","price","yearBuilt","streetAddress","hdpUrl","homeType","daysOnZillow","latitude","longitude","attributionInfo"]
 
-#     with hrequests.BrowserSession(mock_human=True,browser="firefox") as session:
-#         r = session.get(maps)
-#         print(r.status_code)
-#         print(r.url)
-#         html = r.html.text
-#         r = session.get(url)
-#         print(r.status_code)
-#         print(r.url)
-#         html = r.html.text
+     dict_value = [dict_text[item] for item in get_only]
 
+     for i in range(len(get_only)):
+          result[get_only[i]] = dict_value[i]
 
-#     text_dict = json.loads(html)
+          if get_only[i] == "attributionInfo":
+               result["agentName"] = dict_value[i]["agentName"]
+               result["agentPhoneNumber"] = dict_value[i]["agentPhoneNumber"]
+               result["brokerName"] = dict_value[i]["brokerName"]
+               result["brokerPhoneNumber"] = dict_value[i]["brokerPhoneNumber"]
+               result["lastUpdated"] = dict_value[i]["lastUpdated"]
+               del result["attributionInfo"]
+     return result
 
-#     listing_count = text_dict["listing_count"]
-#     listings = text_dict["listings"]
-#     house_list = []
-#     for house in listings:
-#         data = dict()
-#         for item in house:
-#             data[item] = house[item]
-#             if item == "address":
-#                 address = ",".join(house["address"].values())
-#                 data["address"] = address
-#         house_list.append(data)
-#     # print(house_list)
-#     # print(len(house_list))
+def get_zpid_from_map(encodedZuid):
+    url = f"https://www.zillow.com/profile-page/api/public/v1/map-results?encodedZuid={encodedZuid}"    
+    html = get_html(url)
+    list_of_house_urls = get_house_urls(html)
+    print(f"from {encodedZuid} found for-sale/rent: {len(list_of_house_urls)} houses ")
+    return tuple(list_of_house_urls)
+
+def encodedZuid_to_home_details(encodedZuid)->pandas.DataFrame:
+    list_of_house_urls = get_zpid_from_map(encodedZuid)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+          home_details = executor.map(parse_home_details,list_of_house_urls)
+          df = pandas.DataFrame(home_details)
+    return df
+          
+
+if __name__=="__main__":
+     h = encodedZuid_to_home_details("X1-ZUu63jgpzv25u1_2avjk")
+     zpid = 39418271
+     url = f'https://www.zillow.com/homedetails/{zpid}_zpid'
+     #h = parse_home_details(url)
+     h.to_excel("house_listings.xlsx",index=False)
+     
+
+     # text = get_data("https://www.zillow.com/homedetails/5-N-Main-St-Wharton-NJ-07885/2129602173_zpid/")["props"]["pageProps"]["componentProps"]["gdpClientCache"]
+     # dict_text = json.loads(text)
+     # print(dict_text)
